acf5b101faa9148c72efb989312727af2b1f314d
Eugen Bobrowski <ecologistkai@gmail.com>
2016-05-26 19:22:09 +0300

    New js algorithm

--- comments-antispam.php
+++ comments-antispam.php
@@ -71,6 +71,8 @@ if (is_admin()) {
 
         protected static $instance;
         private $nonce;
+        private $localize_object;
+        private $fields;
 
         private function __construct()
         {
@@ -79,8 +81,24 @@ if (is_admin()) {
 
             $this->nonce = hash('md5', ABSPATH);
 
+            $this->localize_object = 'veritas';
+
+            $this->fields = apply_filters('antispam_fields', array(
+                //field name
+                'comment' => array(
+                    //protect method (replace | appending )
+                    'method' => 'replace',
+                    //parent to copy and hide
+                    'parent' => '.comment-form-comment',
+                ),
+            ));
+
+            foreach ($this->fields as $name => $settings) {
+                $this->fields[$name]['ha'] = hash('md5', ABSPATH . $name);
+            }
+
             add_filter('init', array($this, 'verify_spam'));
-            add_filter('comment_form_fields', array($this, 'add_real_comment_field'));
+            add_action('wp_print_scripts', array($this, 'localize'));
             add_filter('print_footer_scripts', array($this, 'javascript'));
             return true;
         }
@@ -100,41 +118,108 @@ if (is_admin()) {
             return $comment_fields;
         }
 
+        public function localize()
+        {
+
+
+            wp_enqueue_script('jquery');
+            wp_localize_script('jquery', $this->localize_object, $this->fields);
+        }
+
         public function javascript()
         {
             wp_enqueue_script('jquery');
+            ob_start();
             ?>
             <script>
                 (function ($) {
                     var $comentField;
                     $(document).ready(function () {
-                        $comentField = $('#comment');
-                        $('#<?php echo $this->nonce; ?>').addClass($comentField.attr('class'));
-                        $('#<?php echo $this->nonce; ?>').parent().addClass($comentField.parent().attr('class'));
-                        $comentField.removeAttr('aria-required').removeAttr('required').parent().hide();
+
+                        var someFunction = function (e) {
+                            console.log(veritas);
+                            for (var key in veritas) {
+                                var $field = $('[name="' + key + '"]');
+                                if ($field.length < 1 ) continue;
+
+                                $field.each(function () {
+                                    var $this = $(this);
+
+                                    var $parent = $this.parents(veritas[key].parent);
+
+                                    if (veritas[key].method = 'replace') {
+                                        $this.attr('id', veritas[key].ha).attr('name', veritas[key].ha);
+                                    } else if (veritas[key].method = 'append') {
+
+                                        $("label[for='" + $this.attr('id') + "']").attr('for', veritas[key].ha);
+                                        $this.attr('id', veritas[key].ha).attr('name', veritas[key].ha);
+                                    }
+
+
+
+                                })
+
+
+
+
+                            }
+
+
+                            $comentField = $('#comment');
+
+                            $('#<?php echo $this->nonce; ?>').addClass($comentField.attr('class'));
+                            $('#<?php echo $this->nonce; ?>').parent().addClass($comentField.parent().attr('class'));
+                            /*$comentField.removeAttr('aria-required').removeAttr('required').parent().hide();*/
+                        };
+
+                        setTimeout(someFunction, 1000);
+
                     });
                 })(jQuery);
 
             </script>
-
-
             <?php
+            $js = ob_get_clean();
+
+            $js = preg_replace('!/\*[^*]*\*+([^/][^*]*\*+)*/!', '', $js);
+            $js = str_replace(': ', ':', $js);
+            $js = str_replace(array("\r\n", "\r", "\n", "\t", '  ', '    ', '    '), '', $js);
+            $obfuscate = array(
+                'someFunction' => 'a',
+            );
+            $js = str_replace(array("\r\n", "\r", "\n", "\t", '  ', '    ', '    '), '', $js);
+            echo($js);
+
         }
+
+
 
         public function verify_spam($commentdata)
         {
-
-            if (isset($_POST['comment'])) {
-                $spam_test_field = trim($_POST['comment']);
-                if (!empty($spam_test_field)) {
-                    $spam_detected = get_option('spams_detected', 0);
-                    $spam_detected++;
-                    update_option('spams_detected', $spam_detected);
-                    wp_die(__('Sorry, comments for bots are closed.'));
+            foreach ($this->fields as $name => $field) {
+                if ('replace' == $field['method']) {
+                    if (!isset($_POST['comment']) && isset($_POST[$field['ha']])) {
+                        $_POST['comment'] = $_POST[$field['ha']];
+                    } else {
+                        $spam_detected = get_option('spams_detected', 0);
+                        $spam_detected++;
+                        update_option('spams_detected', $spam_detected);
+                        wp_die(__('Sorry, comments for bots are closed.'));
+                    }
+                } elseif ('append' == $field['method'] && isset($_POST['comment'])) {
+                    $spam_test_field = trim($_POST['comment']);
+                    if (!empty($spam_test_field)) {
+                        $spam_detected = get_option('spams_detected', 0);
+                        $spam_detected++;
+                        update_option('spams_detected', $spam_detected);
+                        wp_die(__('Sorry, comments for bots are closed.'));
+                    }
+                    $comment_content = trim($_POST[$this->nonce]);
+                    $_POST['comment'] = $comment_content;
                 }
-                $comment_content = trim($_POST[$this->nonce]);
-                $_POST['comment'] = $comment_content;
             }
+
+
             return $commentdata;
         }
 
