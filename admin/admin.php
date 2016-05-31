8e69a16cbda9fe5e1d6fa8861136d0870631890c
Eugen Bobrowski <ecologistkai@gmail.com>
2016-05-31 18:22:30 +0300

    Add tools page

--- admin/admin.php
+++ admin/admin.php
@@ -8,6 +8,8 @@ class Antispam_Admin
     {
         add_action('admin_bar_menu', array($this, 'show_spam_count'), 99);
         add_action('admin_print_styles', array($this, 'style'));
+        add_action('admin_menu', array($this, 'add_tools_submenu'));
+        add_action('admin_enqueue_scripts', array($this, 'enqueue'));
         return true;
     }
 
@@ -17,6 +19,42 @@ class Antispam_Admin
             self::$instance = new self();
         }
         return self::$instance;
+    }
+
+    public function add_tools_submenu()
+    {
+        add_submenu_page(
+            'tools.php',
+            'Antispam statistic and settings',
+            'Antispam',
+            'manage_options',
+            'antispam-statistic',
+            array($this, 'view'));
+    }
+
+    public function view()
+    {
+        ?>
+        <div class="wrap">
+
+            <h1>
+                <?php echo get_admin_page_title(); ?>
+            </h1>
+            <div id="chartContainer" class="antispam-chart" style="height: 300px; width: 100%;">
+
+            </div>
+
+        </div>
+
+
+        <?php
+    }
+
+    public function enqueue($prefix)
+    {
+        if (strpos('tools_page_antispam-statistic', $prefix) === false) return;
+        wp_enqueue_script('canvasjs', plugin_dir_url(__FILE__) . 'canvasjs.min.js?plugin=' . $prefix);
+        wp_enqueue_script('antispam-admin', plugin_dir_url(__FILE__) . 'antispam.js?plugin=' . $prefix, array('jquery', 'canvasjs'));
     }
 
     public function show_spam_count($wp_admin_bar)
@@ -29,7 +67,7 @@ class Antispam_Admin
 
         $wp_admin_bar->add_node(array(
             "id" => "antispam-plugin-counter",
-            "title" =>  get_option('spams_detected', 0) . ' rejected',
+            "title" => get_option('spams_detected', 0) . ' rejected',
             "parent" => "antispam-plugin",
         ));
 
