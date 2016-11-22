aca84030f2d6db962678bac0327ce55b911d3bcd
Eugen Bobrowski <ecologistkai@gmail.com>
2016-11-22 13:11:02 +0200

    Show first day of stat

--- admin/antispam.js
+++ admin/antispam.js
@@ -1,79 +1,98 @@
 /**
  * Created by eugen on 5/31/16.
  */
-window.onload = function () {
-    var chart = new CanvasJS.Chart("chartContainer",
-        {
-            animationEnabled: true,
-            title:{
-                text: "Antispam stistic"
-            },
-            data: [
-                {
-                    type: "spline", //change type to bar, line, area, pie, etc
-                    showInLegend: true,
-                    dataPoints: [
-                        // { label: '1 may', y: 51 },
-                        // { label: '2 may', y: 45},
-                        // { label: '3 may', y: 50 },
-                        // { label: '4 may', y: 62 },
-                        // { label: '5 may', y: 95 },
-                        // { label: '6 may', y: 66 },
-                        // { label: '7 may', y: 24 },
-                        { label: '8 may', y: 32 },
-                        { label: '9 may', y: 16 }
-                    ]
-                },
-                {
-                    type: "spline",
-                    showInLegend: true,
-                    dataPoints: [
-                        // { label: '1 may', y: 31 },
-                        // { label: '2 may', y: 25},
-                        // { label: '3 may', y: 80 },
-                        // { label: '4 may', y: 52 },
-                        // { label: '5 may', y: 65 },
-                        // { label: '6 may', y: 56 },
-                        // { label: '7 may', y: 34 },
-                        { label: '8 may', y: 0 },
-                        { label: '9 may', y: 0 }
-                    ]
-                }
-            ],
-            legend: {
-                cursor: "pointer",
-                itemclick: function (e) {
-                    if (typeof(e.dataSeries.visible) === "undefined" || e.dataSeries.visible) {
-                        e.dataSeries.visible = false;
-                    } else {
-                        e.dataSeries.visible = true;
-                    }
-                    chart.render();
-                }
-            }
-        });
 
-    chart.render();
-};
 
 (function ($) {
 
-    var data = {};
 
-    $(document).ready(function () {
-        console.log(antispam_data);
-        data.rejected = [];
-        var a = antispam_data.data;
-        var first = a.shift();
-        var last = a.pop();
+    window.onload = function () {
 
-        console.log(first, last, a);
+        var points1 = preparedata(antispam_data.data);
 
+        var chart = new CanvasJS.Chart("chartContainer",
+            {
+                animationEnabled: true,
+                title:{
+                    text: "Antispam stistic"
+                },
+                data: [
+                    {
+                        type: "spline", //change type to bar, line, area, pie, etc
+                        showInLegend: true,
+                        dataPoints: points1
+                    }
+                    ,
+                    {
+                        type: "spline",
+                        showInLegend: true,
+                        dataPoints: [
+                            // { label: '1 may', y: 31 },
+                            // { label: '2 may', y: 25},
+                            // { label: '3 may', y: 80 },
+                            // { label: '4 may', y: 52 },
+                            // { label: '5 may', y: 65 },
+                            // { label: '6 may', y: 56 },
+                            // { label: '7 may', y: 34 },
+                            { label: '8 may', y: 0 },
+                            { label: '9 may', y: 0 }
+                        ]
+                    }
+                ],
+                legend: {
+                    cursor: "pointer",
+                    itemclick: function (e) {
+                        if (typeof(e.dataSeries.visible) === "undefined" || e.dataSeries.visible) {
+                            e.dataSeries.visible = false;
+                        } else {
+                            e.dataSeries.visible = true;
+                        }
+                        chart.render();
+                    }
+                }
+            });
 
+        chart.render();
+    };
 
+    var chart = {};
 
+    var preparedata = function (a) {
 
-    });
+        var first = {};
+        first.el = a.shift();
+        first.date = Date.parse(first.el.s_date);
+        var last = {};
+        last.el = a.pop();
+        last.date = Date.parse(last.el.s_date);
+        var timeline = [];
+        var current = {};
+        console.log(first.date, last.date, a);
+
+        current.date = new Date (first.date);
+
+        timeline.push({label: current.date.toLocaleString("en-US", {
+            month: 'short',
+            day: 'numeric'
+        }), y: parseInt(first.el.s_count)});
+
+        current.timestamp = first.date;
+        while (current.timestamp < last.date) {
+            current.timestamp += 3600 * 24 * 1000;
+            current.date = new Date (current.timestamp);
+            timeline.push({
+                label: current.date.toLocaleString("en-US", {
+                month: 'short',
+                day: 'numeric'
+            }), y: 0});
+
+        }
+
+        console.log(timeline);
+
+        return timeline;
+
+    }
 
 
 })(jQuery);
